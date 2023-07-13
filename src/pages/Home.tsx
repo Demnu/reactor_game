2321d58c432d03da25a75a26c109928cf9f04616
Demnu <harrison.joe.collins@gmail.com>
2023-07-13 23:09:28 +1000

    First commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className=" text-center text-3xl mt-2 mb-6">
+        Reactor Control Training Game
+      </h1>
+      <div className=" flex justify-center">
+        <button className=" bg-red-600 h-12 w-40 text-white rounded-sm">
+          Start
+        </button>
+      </div>
+    </div>
+  );
+}
